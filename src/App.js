6f96c326214a22eeeb0de660a922f67b2112f3c1
n1h40 <natiqnihad06@gmail.com>
2024-12-10 11:10:46 +0400

    commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navigation from "./pages/Navigation";
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
